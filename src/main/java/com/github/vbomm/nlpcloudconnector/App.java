15c20b17b177a9f768c918c34d188c97f5f0f3fc
vbomm <vitus.bomm@gmail.com>
2023-02-20 19:40:01 +0100

    added comment, moved question and context variables in try

--- src/main/java/com/github/vbomm/nlpcloudconnector/App.java
+++ src/main/java/com/github/vbomm/nlpcloudconnector/App.java
@@ -6,17 +6,19 @@ import java.io.IOException;
 
 public class App {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Dotenv config = Dotenv.configure().load();
         String token = config.get("NLPCLOUDTOKEN");
 
         NlpCloud nlp = new NlpCloud(token);
 
-        String question = "How many threats are blocked per day?";
-        String context = "We have built security into every component of the network. Cloudflare’s 155 Tbps network blocks an average of 70 billion threats per day, including some of the largest DDoS attacks ever recorded. Each and every login, request, and response that goes through our network strengthens the machine learning that we apply to detect and block threats at the edge, before they ever reach your organization.";
-
+        // example
         try {
+            String question = "How many threats are blocked per day?";
+            String context = "We have built security into every component of the network. Cloudflare’s 155 Tbps network blocks an average of 70 billion threats per day, including some of the largest DDoS attacks ever recorded. Each and every login, request, and response that goes through our network strengthens the machine learning that we apply to detect and block threats at the edge, before they ever reach your organization.";
+
             String answer = nlp.question(Model.robertaBaseSquad2(), false, question, context);
+
             System.out.println(answer);
         } catch (IOException e) {
             throw new RuntimeException(e);
