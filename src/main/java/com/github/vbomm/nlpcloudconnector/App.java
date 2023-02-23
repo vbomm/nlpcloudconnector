165054880a9e6d54314901ae5d9cd0b5f6234e8c
vbomm <vitus.bomm@gmail.com>
2023-02-23 18:17:03 +0100

    added example for generations

--- src/main/java/com/github/vbomm/nlpcloudconnector/App.java
+++ src/main/java/com/github/vbomm/nlpcloudconnector/App.java
@@ -12,12 +12,23 @@ public class App {
 
         NlpCloud nlp = new NlpCloud(token);
 
-        // example
+        // example question
         try {
             String question = "How many threats are blocked per day?";
             String context = "We have built security into every component of the network. Cloudflare’s 155 Tbps network blocks an average of 70 billion threats per day, including some of the largest DDoS attacks ever recorded. Each and every login, request, and response that goes through our network strengthens the machine learning that we apply to detect and block threats at the edge, before they ever reach your organization.";
 
             String answer = nlp.question(Model.robertaBaseSquad2(), false, question, context);
+
+            System.out.println(answer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // example generation
+        try {
+            String text = "GPT is a powerful NLP model ";
+
+            String answer = nlp.generation(Model.finetunedGptNeox20b(), true, text, 50);
 
             System.out.println(answer);
         } catch (IOException e) {
